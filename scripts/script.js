5257f3377b924083d593d40e7c4293beadb2fbae
Your Name <you@example.com>
2023-03-27 17:41:53 +0300

    fix form sending

--- scripts/script.js
+++ scripts/script.js
@@ -29,6 +29,9 @@ function render_cvss_rating(){
   page_cvss_score.textContent = cvss_score.textContent
   page_cvss_severity.textContent = cvss_severity.textContent
   page_vector_string.value = cvss_vector_string.value
+  page_vector_string.onchange = function() {
+    window.location.href =  str.replace(/.html#/gi, page_vector_string.value);
+  }
   setCritical(page_cvss_score.textContent)
 }
 
@@ -139,21 +142,19 @@ function addLink() {
 
 // =============== работа с свидетельствами ===============
 // добавление файлов
-let inputs = document.querySelectorAll('.input__file');
-    Array.prototype.forEach.call(inputs, function (input) {
-      let label = input.nextElementSibling,
-        labelVal = label.querySelector('.input__file-button-text').innerText;
-  
-      input.addEventListener('change', function (e) {
-        if (this.files && this.files.length >= 1)
-          fileName = this.files[0].name;
+function uploadFile(file,file_input,label,labelVal) {
+  if (!['image/jpeg', 'image/png', 'image/gif'].includes(file.type)) {
+    alert('Разрешены только изображения!')
+    file_input.value = '';
+    return
+  }
 
-        if (fileName)
-          label.querySelector('.input__file-button-text').innerText = fileName;
-        else
-          label.querySelector('.input__file-button-text').innerText = labelVal;
-      });
-    });
+  if (file.name)
+    label.querySelector('.input__file-button-text').innerText = file.name;
+  else
+    label.querySelector('.input__file-button-text').innerText = labelVal;
+  previewImage(file_input)
+}
 
 // превью картинок
 function previewImage(element) {
@@ -167,7 +168,6 @@ function previewImage(element) {
     let event_link;
     reader.onload = function(event) {
       preview_img.setAttribute("src",event.target.result)
-      //preview_img.style.background = `url("${event.target.result}")`
       preview_img.parentElement.style.display = "block";
       event_link = event.target.result
     }
@@ -234,31 +234,40 @@ window.onclick = function(event) {
 }
 
 // создание блока
+let i = 0; // инкрементное значение для id файловых импутов
 function createBlock(selectedValue) {
   const div = document.createElement('div')
   div.setAttribute('draggable','true')
   if (selectedValue == 'description_block_option') { // блок с описанием
     div.setAttribute('class','description_block detail_block')
     div.innerHTML = `<div class="move_icon"></div>
-        <textarea name="description" id="" cols="30" rows="2" onkeyup="textAreaAdjust(this)"></textarea> 
+        <textarea data-i="descr-${i}" name="description" id="" cols="30" rows="2" onkeyup="textAreaAdjust(this)"></textarea> 
         <input type="button" class="btn" required value="Добавить блок" onclick="openModalAddBlock()">
         <div class="del_icon" onclick="this.parentElement.remove()"></div>`
     modalDetail.style.display = "none"
+    i++
   } else if (selectedValue == 'image_block_option') { // блок с изображением
     div.setAttribute('class','image_block detail_block')
     div.innerHTML = `<div class="move_icon"></div>
       <div class="input__wrapper">
-          <input name="file" type="file" required accept="image/*" id="input__file" class="input input__file" onchange="previewImage(this)">
-          <label for="input__file" class="input__file-button btn">
+          <input name="file" type="file" data-i="file_input-${i}" required accept=".jpg, .png, .gif" id="input__file-${i}" class="input input__file">
+          <label for="input__file-${i}" class="input__file-button btn">
               <span class="input__file-icon-wrapper"><img class="input__file-icon" src="./img/iconfinder_download_down_save_8666778.svg" alt="Выбрать файл" width="25"></span>
               <span class="input__file-button-text">Выберите файл</span>
           </label>
           <div class="preview_img_div"><img src="#" class="preview_img"></div>
           </div>
-      <textarea name="image_descr" id="" cols="30" rows="2" required onkeyup="textAreaAdjust(this)"></textarea>
+      <textarea name="image_descr" data-i="image_descr-${i}" id="" cols="30" rows="2" required onkeyup="textAreaAdjust(this)"></textarea>
       <input type="button" class="btn" value="Добавить блок" onclick="openModalAddBlock()">
       <div class="del_icon" onclick="this.parentElement.remove()"></div>`
     modalDetail.style.display = "none"
+    i++
+    div.querySelector('input[type="file"]').addEventListener('change', function (e) {
+      console.log(e)
+      let label = e.target.nextElementSibling
+      let labelVal = label.querySelector('.input__file-button-text').innerText;
+      uploadFile(e.target.files[0], e.target, label, labelVal) 
+    })
   }
   return div
 }
@@ -273,31 +282,22 @@ function addBlock(selectedBlock) {
 async function handleFormSubmit(event) {
   event.preventDefault()
 
+
   const data = serializeForm(event.target)
   console.log(data)
+  applicantForm.classList.add('_sending')
+  
   const response = await sendData(data)
 }
 
+
 // сбор данных с формы
 function serializeForm(formNode) {
-  // const { elements } = formNode
-  // const data = Array.from(elements)
-  //   .filter((item) => !!item.name)
-  //   .map((element) => {
-  //     const { name } = element
-  //   if (name == 'vuln-classification') {
-  //     element.value = tags
-  //   }
-  //   const value = element.value
-  //   input.value = ''
-  //     return { name, value }
-  //   })
-
-  // console.log(data)
 
   const { elements } = formNode
 
   const data = new FormData()
+  const fileUploadBlocks = document.querySelectorAll('.image_block');
 
   Array.from(elements)
     .filter((item) => !!item.name)
@@ -309,7 +309,15 @@ function serializeForm(formNode) {
       const value = element.value
       data.append(name, value)
     })
-    input.value = ''
+
+  fileUploadBlocks.forEach(block => {
+    const fileInput = block.querySelector('input[type="file"]');
+    const fileDesc = block.querySelector('textarea');
+
+    data.append(fileInput.name, fileInput.files[0]);
+    data.append(fileDesc.name, fileDesc.value);
+  });
+  input.value = ''
   return data
 }
 
@@ -317,9 +325,16 @@ function serializeForm(formNode) {
 async function sendData(data) {
   return await fetch('/api/apply/', {
     method: 'POST',
-    headers: { 'Content-Type': 'multipart/form-data' },
     body: data,
-  })
+  }).then(response => {
+    if (response.ok) {
+      form.reset()
+      form.classList.remove('_sending')
+    }
+  }).catch(error => {
+    alert('Ошибка отправки' + error)
+    form.classList.remove('_sending')
+  });
 }
 
 
